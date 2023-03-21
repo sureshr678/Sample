5fc7ad9a146dd2fc83592242d36794111ec2aaf0
sureshr678 <sureshdevops.r@gmail.com>
2023-03-21 22:26:55 +0530

    addingTerraformfile

---
+++ Terraform-learn/test.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0089b8e98cd95257d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0bc998f018bc2e148"]
+  tags = {
+    Name = "test"
+  }
+}
