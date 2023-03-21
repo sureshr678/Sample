db107fceca6247adabd27ee1d8f47b32e2bdde23
sureshr678 <sureshdevops.r@gmail.com>
2023-03-21 23:01:13 +0530

    updtefile

--- Terraform-learn/test.tf
+++ Terraform-learn/test.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0089b8e98cd95257d"
+  ami                    = "ami-0bb6af715826253bf"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0bc998f018bc2e148"]
   tags = {
