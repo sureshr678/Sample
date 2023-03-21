5261cfae826d9b4b226f9e3c9b549aa57e8d31e4
sureshr678 <sureshdevops.r@gmail.com>
2023-03-21 23:33:28 +0530

    updated

--- Terraform-learn/test.tf
+++ Terraform-learn/test.tf
@@ -1,3 +1,11 @@
+
+
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+
+
 resource "aws_instance" "ec2" {
   ami                    = "ami-0bb6af715826253bf"
   instance_type          = "t3.micro"
