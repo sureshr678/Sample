263423b7ee8674af0a6c051dea62cd4283b603fc
sureshr678 <sureshdevops.r@gmail.com>
2023-03-23 17:53:30 +0530

    added ami

--- Terraform-learn/test.tf
+++ Terraform-learn/test.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0bb6af715826253bf"
+  ami                    = "ami-0089b8e98cd95257d"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0bc998f018bc2e148"]
   tags = {
