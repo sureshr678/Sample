231f5bb2f9eb9964d72a9d0989be6eee0c3545d8
sureshr678 <sureshdevops.r@gmail.com>
2023-03-23 17:56:45 +0530

    addedtag

--- Terraform-learn/test.tf
+++ Terraform-learn/test.tf
@@ -12,6 +12,6 @@ resource "aws_instance" "ec2" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0bc998f018bc2e148"]
   tags = {
-    Name = "test"
+    Name = "test1"
   }
 }
