b17e477e85deb20d51430e2c23fdfe9959084d70
sureshr678 <sureshdevops.r@gmail.com>
2023-03-26 19:59:59 +0530

    updated

--- Terraform-learn/test.tf
+++ Terraform-learn/test.tf
@@ -1,9 +1,22 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0089b8e98cd95257d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0bc998f018bc2e148"]
+  tags = {
+    Name = "test"
+  }
 }
+ provisioner "remote-exec" {
 
-output "ami_id" {
-  value = data.aws_ami.ami.image_id
+    connection {
+      host     = self.public_ip
+      user     = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "echo Hello"
+    ]
+  }
+
 }
